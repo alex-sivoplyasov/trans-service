879a0975310e932983c7a46c4259ac31f9fd46ce
Alex Sivoplyasov <alex_sivoplyasov@mail.ru>
2019-01-12 20:56:49 +0500

    commit

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,13 +1,44 @@
-var el = document.querySelector('.header__item');
-// console.log( el.classList);
-// el.classList.add('active');
-// console.log(el);
-//
-var el = document.querySelector('.primaryButton');
-
-el.addEventListener('click', onClickPrimaryButton);
-
+let  elnn = document.querySelector('.primaryButton');
+elnn.addEventListener('click', onClickPrimaryButton);
+//elm = document.querySelector('.nav__item');
+var isActive = false;
 function  onClickPrimaryButton() {
-    el.classList.add('active');
+    let  elm = document.querySelectorAll('.nav__item');
+    let test = document.querySelector('.nav__container');
+    let list = document.querySelector('.nav__list');
+    // elm.classList.add('nav__is-active');
+    // for(i =0; i<elm.length; i++)
+    // {
+    //     elm[i].classList.add('nav__is-active');
+    // }
+    // console.log(elm);
 
+   let rezult = Array.from(elm);
+    console.log(rezult);
+    if(!isActive) {
+        for(i = 0; i < rezult.length; i++)
+        {
+            rezult[i].classList.add('nav__is-active');
+        }
+        isActive = true;
+        console.log("First value: " + isActive);
+
+
+        console.log(test);
+         test.classList.add('nav__is-active');
+        list.classList.add('nav__is-active');
+        console.log(test);
+    } else {
+        for(i = 0; i < rezult.length; i++)
+        {
+            rezult[i].classList.remove('nav__is-active');
+        }
+        test.classList.remove('nav__is-active');
+        list.classList.remove('nav__is-active');
+
+        isActive = false;
+        console.log("Second value: " + isActive);
+
+    }
 }
+
