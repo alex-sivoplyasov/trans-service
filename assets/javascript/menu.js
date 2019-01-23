a77b2e844e0aa9411b4fc1f2a829eff4a8981573
Alex Sivoplyasov <alex_sivoplyasov@mail.ru>
2019-01-23 11:43:20 +0500

    Version with new style

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,44 +1,19 @@
-let  elnn = document.querySelector('.primaryButton');
-elnn.addEventListener('click', onClickPrimaryButton);
-//elm = document.querySelector('.nav__item');
-var isActive = false;
-function  onClickPrimaryButton() {
-    let  elm = document.querySelectorAll('.nav__item');
-    let test = document.querySelector('.nav__container');
-    let list = document.querySelector('.nav__list');
-    // elm.classList.add('nav__is-active');
-    // for(i =0; i<elm.length; i++)
-    // {
-    //     elm[i].classList.add('nav__is-active');
-    // }
-    // console.log(elm);
+let button = document.querySelector('.nav__burger-wrapper');
+console.log(button);
+button.addEventListener('click', onClickPrimaryButton);
+let  isActive = false;
 
-   let rezult = Array.from(elm);
-    console.log(rezult);
+function onClickPrimaryButton() {
+    let element = document.querySelector('.nav__list');
+    // console.log("First " + element);
+    //
+    // console.log("Second " + element);
+    console.log(isActive);
     if(!isActive) {
-        for(i = 0; i < rezult.length; i++)
-        {
-            rezult[i].classList.add('nav__is-active');
-        }
+        element.classList.add('nav__is-active');
         isActive = true;
-        console.log("First value: " + isActive);
-
-
-        console.log(test);
-         test.classList.add('nav__is-active');
-        list.classList.add('nav__is-active');
-        console.log(test);
     } else {
-        for(i = 0; i < rezult.length; i++)
-        {
-            rezult[i].classList.remove('nav__is-active');
-        }
-        test.classList.remove('nav__is-active');
-        list.classList.remove('nav__is-active');
-
+        element.classList.remove('nav__is-active');
         isActive = false;
-        console.log("Second value: " + isActive);
-
     }
 }
-
